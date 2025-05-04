6408d5f41d1b2dd9636c1840fd6c0f2c5c588328
Hugo <149089365+humarroquin@users.noreply.github.com>
2025-05-03 20:04:33 -0600

    updated the bmi calculation in the js file

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 "use strict";
 
-// metric
 const height = document.getElementById("height");
 const weight = document.getElementById("weight");
 const metricResult = document.querySelector(".metric-result");
@@ -8,29 +7,49 @@ const welcomeMessage = document.querySelector(".welcome-message");
 const metricMessage = document.querySelector(".metric-message");
 const BMIDescription = document.querySelector(".BMI-description");
 
-let weight1, weight2;
+function updateUI() {
+  const heightVal = Number(height.value);
+  const weightVal = Number(weight.value);
 
-function metricBMICalculation() {
-  if (!weight.value || !height.value || height.value < 100) {
+  // don't update on empty inputs or height numbers below 100
+  if (!weightVal || !heightVal || heightVal < 100) {
     welcomeMessage.classList.remove("hidden");
     metricMessage.classList.add("hidden");
     return;
   }
 
+  // proceed to update UI with BMI result and message
   welcomeMessage.classList.add("hidden");
   metricMessage.classList.remove("hidden");
 
-  const heightInMetersValue = Number(height.value) / 100;
-  const weightValue = Number(weight.value);
-  const BMI = weightValue / heightInMetersValue ** 2;
-  metricResult.textContent = BMI.toFixed(1);
-  const idealWeight = BMI * heightInMetersValue ** 2;
+  // convert to numbers and height to meters
+  const heightInMetersValue = heightVal / 100;
 
-  // todo: calculate ranges: "Your ideal weight is between 63.3kgs - 85.2kgs."
-  BMIDescription.textContent = `Your BMI suggests that you have a ${
-    BMI >= 18.5 && BMI <= 24.9 ? "healthy" : "unhealthy"
-  } weight. Your ideal weight should be ${idealWeight.toFixed(1)}kgs.`;
+  function calculateBMI() {
+    return weightVal / heightInMetersValue ** 2;
+  }
+
+  function getWeightRange(bmi) {
+    return bmi * heightInMetersValue ** 2;
+  }
+
+  const BMI = calculateBMI();
+  const minWeight = getWeightRange(18.5);
+  const maxWeight = getWeightRange(24.9);
+  const isHealthy = BMI >= 18.5 && BMI <= 24.9;
+
+  metricResult.textContent = BMI.toFixed(1);
+
+  BMIDescription.textContent = `Your BMI suggests that you have ${
+    isHealthy ? "a healthy" : "an unhealthy"
+  } weight. ${
+    isHealthy
+      ? `Your ideal weight should be between ${minWeight.toFixed(
+          1
+        )}kg and ${maxWeight.toFixed(1)}kg`
+      : ""
+  }`;
 }
 
-weight.addEventListener("input", metricBMICalculation);
-height.addEventListener("input", metricBMICalculation);
+weight.addEventListener("input", updateUI);
+height.addEventListener("input", updateUI);
